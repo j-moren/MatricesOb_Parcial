040a5a99e3fc27e52d964cf1e0eb4ca431bb9282
J-Moren <kalev.1503@gmail.com>
2025-03-02 17:34:21 -0500

    Se crea Método Punto3

--- Punto3.java
+++ Punto3.java
@@ -1,3 +1,8 @@
 public class Punto3 {
-    
+    public void P3 (int d) {
+        Metodos m = new Metodos();
+        Producto[][] M = new Producto [d][d];
+        M = m.llenarMatriz(d);
+        m.libroMasCostoso(M);
+    }
 }
