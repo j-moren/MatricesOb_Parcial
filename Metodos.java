f21ebcaf1bc011772ed138cde1ed1e995b83909d
J-Moren <kalev.1503@gmail.com>
2025-03-02 15:37:48 -0500

    Se Crea Método Llenar Matriz y se crea el objeto con sus parametos

--- Metodos.java
+++ Metodos.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
 public class Metodos {
+    Scanner sc = new Scanner (System.in);
+
+    public Producto[][] llenarMatriz(int dimension) {
+        Producto[][] m = new Producto [dimension][dimension];
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                Producto o = new Producto();
+                System.out.println("Ingrese el Titulo del Libro:");
+                o.setTitulo(sc.next());
+                System.out.println("Ingrese el Autor del Libro:");
+                o.setAutor(sc.next());
+                System.out.println("Ingrese el año del Libro:");
+                o.setAño(sc.nextInt());
+                System.out.println("Ingrese el Precio del Libro:");
+                o.setPrecio(sc.nextDouble());
+                m[i][j] = o;
+            }
+        }
+        return m;
     
+    }
 }
